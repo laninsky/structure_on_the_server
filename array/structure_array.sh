7019ba1bd82d8a741de401287d88a4c52a786d9a
Alana Alexander <laninsky@gmail.com>
2015-05-27 14:19:41 -0500

    Update structure_array.sh

--- array/structure_array.sh
+++ array/structure_array.sh
@@ -1,1 +1,11 @@
+#PBS -N job_name
+#PBS -l nodes=1:ppn=1:avx,mem=5000m,walltime=4:00:00
+#PBS -M your_email_address
+#PBS -t 0-19
+#PBS -r n
+#PBS -m n
+#PBS -j oe
+#PBS -o /dev/null
+#PBS -d /scratch/pathway_to_your_data_and_params_files
 
+structure -m your_param_file_for_specific_k -o k_specific_k_${PBS_ARRAYID} > ${PBS_O_WORKDIR}/$PBS_JOBNAME.log
